9e060afb82cd509dc1140381ae0c7e7670f360f7
kwongeunwoo <gunwoo00@hanyang.ac.kr>
2023-12-31 18:04:50 +0900

    Title: 지그재그로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    int num = 1;
+
+    cin >> n >> m;
+
+    // n*n vector를 모두 0으로 초기화
+    vector<vector<int>> array(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i % 2 == 0)
+            {
+                array[j][i] = num;
+                num++;
+            }
+            else if (i % 2 == 1)
+            {
+                array[n - 1 - j][i] = num;
+                num++;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
