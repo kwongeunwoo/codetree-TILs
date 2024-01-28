2929130388d8804c91a3f5e97cfe1f119fe27e6b
kwongeunwoo <gunwoo00@hanyang.ac.kr>
2024-01-29 00:19:34 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/동전이 있는 위치/Where-coins-are-located.cpp
+++ 240129/동전이 있는 위치/Where-coins-are-located.cpp
@@ -4,38 +4,23 @@ using namespace std;
 
 int main()
 {
-    int n, m;
-    n = 0;
-    m = 0;
+    int n,m;
     cin >> n >> m;
-    vector<int> r(m, 0);
-    vector<int> c(m, 0);
-    int m_count = 0;
 
-    vector<vector<int>> array(n, vector<int>(n, 0));
+    vector<vector<int>> matrix (n,vector<int>(n,0));
 
-    for (int i = 0; i < m; i++)
+    int r,c;
+    for(int i = 0; i<m; i++)
     {
-        cin >> r[i] >> c[i];
+        cin >> r >> c;
+        matrix[r-1][c-1] = 1;
     }
 
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i<n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for(int j = 0; j<n; j++)
         {
-            if (i == r[m_count] - 1 && j == c[m_count] - 1 && m_count < m)
-            {
-                array[i][j] = 1;
-                m_count++;
-            }
-        }
-    }
-
-    for (const auto &row : array)
-    {
-        for (const auto &value : row)
-        {
-            cout << value << " ";
+            cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
