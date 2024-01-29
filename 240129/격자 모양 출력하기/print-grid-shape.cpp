6750ad67d5604b7be79fda2b4e673027f3bb82b7
kwongeunwoo <gunwoo00@hanyang.ac.kr>
2024-01-29 18:03:53 +0900

    Title: 격자 모양 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+
+    vector<vector<int>> matrix (n,vector<int>(n,0));
+
+    int r,c;
+    for(int i = 0; i<m; i++)
+    {
+        cin >> r >> c;
+        matrix[r-1][c-1] = r * c;
+    }
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
