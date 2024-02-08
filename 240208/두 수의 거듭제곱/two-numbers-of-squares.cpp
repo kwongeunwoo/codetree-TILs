860bb6e0962bdebe3d79c538b5af22d15e1721bb
kwongeunwoo <gunwoo00@hanyang.ac.kr>
2024-02-08 16:43:08 +0900

    Title: 두 수의 거듭제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240208/두 수의 거듭제곱/two-numbers-of-squares.cpp
+++ 240208/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    int value = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        value = value * a;
+    }
+    cout << value << endl;
     return 0;
 }
